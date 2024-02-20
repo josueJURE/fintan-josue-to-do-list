d1eda7d15bebcf09d9a6fc6aad2c498a13e527b9
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-02-20 11:57:15 +0000

    create Todo component & import into App.jsx

---
+++ src/components/Todo.jsx
@@ -0,0 +1,17 @@
+export default function Todo(props) {
+    return (
+        <li className="todo stack-small">
+            <div className="c-cb">
+                <input id="todo-0" type="checkbox" defaultChecked />
+                <label className="todo-label" htmlFor="todo-0">
+                    Eat
+                </label>
+            </div>
+            <div className="btn-group">
+                <button type="button" className="btn btn__danger">
+                    Delete <span className="visually-hidden">Eat</span>
+                </button>
+            </div>
+        </li>
+    );
+}
