2e080863d7217e759127fca8fd4bc02f4a07b0f2
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-02-22 15:27:19 +0000

    adds handleChange function to store user's input from text box
    uses useState to create a variable, 'name', to register(?) user input
    
    useState also creates setName function to update 'name' with user's input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 export default function Form(props) {
+    const [name, setName] = useState('');
+
+    function handleChange(e) {
+        setName(e.target.value);
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
         // .addTask() is essentially a method for props as it is the function
         // that is being passed to '<Form />' in App.jsx which is then available
-        // here via props/as a prop. 'say hell' is the value being passed to the 'addTask()'
+        // here via props/as a prop. 'name' variable is the value being passed to the 'addTask()'
         // functions that is "stored" in props (probably definitely the incorrect terminology)
-        props.addTask('say hell');
+        props.addTask(name);
+        // call setName with empty string to reset the input box 
+        setName('');
     }
+
     return (
         <form onSubmit={handleSubmit}>
             <h2 className="label-wrapper">
@@ -22,6 +32,8 @@ export default function Form(props) {
                 className="input input__lg"
                 name="text"
                 autoComplete="off"
+                value={name}
+                onChange={handleChange}
             />
             <button type="submit" className="btn btn__primary
             btn__lg">
