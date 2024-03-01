843c2b9ec88f4c2377ebb5852b362c3428e496a0
josueJURE <josue.jure@gmail.com>
2024-03-01 15:16:14 +0000

    (feat:JS): delete unused function

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ import FilterButton from "./components/FilterButton";
 import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 
+const DEFAULT_TASKS = [
+  { id: "todo-0", name: "eat", completed: true },
+  { id: "todo-1", name: "sleep", completed: false },
+  { id: "todo-2", name: "repeat", completed: false },
+];
+
 // connect to local storage
 // pull data from local storage
 // add data to local storage
 
-function connectLocalStorage(task) {
+const updateLocalStorage = (task) => {
   localStorage.setItem("task", JSON.stringify(task));
-}
+};
+
+
 
 // the properties for each key in this object are functions which will be used
 // to filter the 'tasks' (being passed from main.jsx through 'props') data array
 const FILTER_MAP = {
-  All : () => true,
+  All: () => true,
   Active: (task) => !task.completed,
   Completed: (task) => task.completed,
 };
@@ -23,82 +31,107 @@ const FILTER_MAP = {
 // will return an array of the 'key' names from FILTER_MAP
 const FILTER_NAMES = Object.keys(FILTER_MAP);
 
-export default function App(props) {
+export default function App() {
+  // this will preserve the initial value of props in the 'tasks' variable using the
+  // useState() 'hook' to return an array - 'tasks' - which
+  const [tasks, setTasks] = useState();
+  const [filter, setFilter] = useState("All");
 
-// this will preserve the initial value of props in the 'tasks' variable using the 
-// useState() 'hook' to return an array - 'tasks' - which 
-const [tasks, setTasks] = useState(props.tasks);
-const [filter, setFilter] = useState('All');
+  useEffect(() => {
+    const tasksString = localStorage.getItem("task");
+    console.log("tasksString:", tasksString);
+    if (tasksString) {
+      setTasks(JSON.parse(tasksString));
+    } else {
+      setTasks(DEFAULT_TASKS);
+    }
+  }, []);
 
-useEffect(() => {
-  if (tasks) {
-    connectLocalStorage(tasks)
+  useEffect(() => {
+    // tasks is undefined by default, avoid using it until initialized in the above useEffect
+    if (tasks) {
+      updateLocalStorage(tasks);
+    }
+  }, [tasks]);
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same id as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object whose 'completed' prop is inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
   }
-}, [tasks]);
 
-function toggleTaskCompleted(id) {
-  const updatedTasks = tasks.map((task) => {
-    // if this task has the same id as the edited task
-    if (id === task.id) {
-      // use object spread to make a new object whose 'completed' prop is inverted
-      return {...task, completed: !task.completed};
-    }
-    return task;
-  });
-  setTasks(updatedTasks);
-}
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
 
-function deleteTask(id) {
-  const remainingTasks = tasks.filter((task) => id !== task.id);
-  setTasks(remainingTasks);
-}
+  function editTask(id, newName) {
+    const editTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        // copy the task and update the name key with the newName argument
+        return { ...task, name: newName };
+      }
 
-function editTask(id, newName) {
-  const editTaskList = tasks.map((task) => {
-    if (id === task.id) {
-      
-      // copy the task and update the name key with the newName argument
-      return {...task, name: newName};
-    }
-    
-    return task;
-  });
-  
-  setTasks(editTaskList);
-}
+      return task;
+    });
 
-// this passes the relevant properties from 'tasks' into the Todo component as props
-// and filters by the 'filter' variable
-  const taskList = tasks
-  .filter(FILTER_MAP[filter])
-  .map((task) => (
-    <Todo 
-      id={task.id} 
-      name={task.name} 
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
-    />
-  ));
+    setTasks(editTaskList);
+  }
+
+  // this passes the relevant properties from 'tasks' into the Todo component as props
+  // and filters by the 'filter' variable
+
+  // const taskList = tasks
+  // .filter(FILTER_MAP[filter])
+  // .map((task) => (
+  //   <Todo
+  //     id={task.id}
+  //     name={task.name}
+  //     completed={task.completed}
+  //     key={task.id}
+  //     toggleTaskCompleted={toggleTaskCompleted}
+  //     deleteTask={deleteTask}
+  //     editTask={editTask}
+  //   />
+  // ));
+
+  console.log("tasks:", tasks);
+
+  const taskList = (tasks || [])
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
 
   const filterList = FILTER_NAMES.map((name) => (
-    <FilterButton 
-      key={name} 
-      name={name} 
-      isPressed={name === filter} 
-      setFilter={setFilter} 
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
-  
+
   function addTask(name) {
     // nanoid is a JS library designed to create unique identifiers
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     // this uses the spread syntax to copy the information from the array
     // 'tasks' and add it to the object 'newTask'
-    connectLocalStorage(tasks);
-    setTasks([...tasks, newTask])
+    setTasks([...tasks, newTask]);
   }
 
   return (
@@ -107,20 +140,17 @@ function editTask(id, newName) {
       {/* sending addTask() as a prop 'addTask' to Form means addTask() can
       be called in the Form component and pass back data as the 'name' argument
       to be used in the App component. */}
-      <Form addTask={addTask}/>
+      <Form addTask={addTask} />
 
-      <div className="filters btn-group stack-exception">
-        {filterList}
-      </div>
+      <div className="filters btn-group stack-exception">{filterList}</div>
       <h2 id="list-heading"> {taskList.length} tasks remaining</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading">
-          {taskList}
+        aria-labelledby="list-heading"
+      >
+        {taskList}
       </ul>
     </div>
   );
-
-
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './App.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./App.css";
 
-const DATA = [
-  { id: "todo-0", name: "eat", completed: true },
-  { id: "todo-1", name: "sleep", completed: false },
-  { id: "todo-2", name: "repeat", completed: false },
-];
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-{/* passing the DATA array as a prop to the App component so it can be
+    {/* passing the DATA array as a prop to the App component so it can be
 accessed by App.jsx to use the information stored in the array */}
-    <App tasks={DATA}/>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
